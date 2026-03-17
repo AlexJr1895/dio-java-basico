fe4b99272b9d17efec8d0757b9a0c7c5188cb44b
AlexJr1895 <Alexjunior1895@icloud.com>
2026-03-16 22:47:12 -0300

    estruturas de repeticao

---
+++ java-anatomia-classes/src/dio/alex/contacarneirinho/src/ContadorCarneirinho.java
@@ -0,0 +1,11 @@
+public class ContadorCarneirinho {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+//        int carneirinho;
+
+        for( int carneirinho = 1;carneirinho <= 20; carneirinho++){
+            System.err.println("Carneirinhos: "+carneirinho);
+        }
+    }
+}
